71a6eaa6eed5597da49cf488db110eb18bfde4e3
rodriguezlucas1984 <56488774+rodriguezlucas1984@users.noreply.github.com>
2021-07-20 08:53:18 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyB5jpLKOYzq6mWDUkbi1zd3Uxpn7hK5c4c",
-    authDomain: "green-globe-29a96.firebaseapp.com",
-    databaseURL: "https://green-globe-29a96.firebaseio.com",
-    projectId: "green-globe-29a96",
-    storageBucket: "green-globe-29a96.appspot.com",
-    messagingSenderId: "568971004786",
-    appId: "1:568971004786:web:e8096807b49c6d11116beb",
-    measurementId: "G-5DF2B0YFP7"
-  }
 };
 
 /*
